09ddc716af7337a1ff0f002ecf6b6fe3ebcb18af
MD Pahlovi <mdpahlovi07@gmail.com>
2022-10-11 11:51:43 +0600

    Blog Added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,65 @@
 import React from "react";
 
 const Blog = () => {
-    return <div>This is Blog</div>;
+    const exOfUseRef = " <div ref={myRef} />";
+    return (
+        <section className="mt-16 lg:mt-20 my-container section-gap content-gap">
+            <h1 className="text-5xl font-bold text-center">
+                Our Letest <span className="text-primary">Blogs</span>
+            </h1>
+            <p className="max-w-lg mx-auto text-center">
+                you can read our blog and improve your skills whenever you choose to do so. You can also learn new
+                thinks.
+            </p>
+            <article className="max-w-2xl mx-auto content-gap">
+                <div className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl font-bold flex justify-between">
+                        What is the purpose of react router ?
+                    </div>
+                    <div className="collapse-content">
+                        <p>
+                            <span className="font-bold">Ans: </span>
+                            React Router is a standard library for routing in React. It enables the navigation among
+                            views of various components in a React Application, allows changing the browser URL, and
+                            keeps the UI in sync with the URL.
+                        </p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl font-bold flex justify-between">
+                        How does context api works ?
+                    </div>
+                    <div className="collapse-content">
+                        <p>
+                            <span className="font-bold">Ans: </span>
+                            The React Context API is a way for a React app to effectively produce global variables that
+                            can be passed around. This is the alternative to "prop drilling" or moving props from
+                            grandparent to child to parent, and so on. Context is also touted as an easier, lighter
+                            approach to state management using Redux.
+                        </p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl font-bold flex justify-between">
+                        What is useref in react ?
+                    </div>
+                    <div className="collapse-content">
+                        <p>
+                            <span className="font-bold">Ans: </span>
+                            useRef is like a “box” that can hold a mutable value in its .current property. You might be
+                            familiar with refs primarily as a way to access the DOM. If you pass a ref object to React
+                            with
+                            {exOfUseRef} , React will set its .current property to the corresponding DOM node whenever
+                            that node changes.
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </section>
+    );
 };
 
 export default Blog;
--- src/components/QuizDetails.js
+++ src/components/QuizDetails.js
@@ -3,29 +3,37 @@ import { useLoaderData } from "react-router-dom";
 
 const QuizDetails = () => {
     const { data } = useLoaderData();
-    const { questions } = data;
+    const { name, questions } = data;
     return (
-        <div className="my-5 flex flex-col gap-5 max-w-3xl mx-auto pt-16 lg:pt-20">
-            {questions.map((question, index) => (
-                <div className="bg-base-100 px-5 py-5 shadow-sm shadow-base-content rounded-xl flex flex-col items-start w-full">
-                    <h4 className="mt-1 text-xl text-base-content/60">
-                        Question {index + 1} of {questions.length}
-                    </h4>
-                    <div className="my-4 text-2xl">{question.question}</div>
-                    <div className="w-full flex flex-col gap-4">
-                        {question.options.map((answer, index) => (
-                            <div
-                                key={index}
-                                className="flex items-center py-4 px-5 space-x-2 border-2 bg-base-content/5 border-base-content/10 rounded-xl"
-                            >
-                                <input type="radio" className="w-6 h-6" />
-                                <p>{answer}</p>
-                            </div>
-                        ))}
+        <section className="mt-16 lg:mt-20 my-cotainer section-gap content-gap">
+            <h1 className="text-5xl font-bold text-center">
+                Quiz Of <span className="text-primary">{name}</span>
+            </h1>
+            <div className="my-5 content-gap max-w-3xl mx-auto">
+                {questions.map((question, index) => (
+                    <div
+                        key={index}
+                        className="bg-base-100 px-5 py-5 shadow-sm shadow-base-content rounded-xl flex flex-col items-start w-full"
+                    >
+                        <h4 className="mt-1 text-xl text-base-content/60">
+                            Question {index + 1} of {questions.length}
+                        </h4>
+                        <div className="my-4 text-2xl">{question.question}</div>
+                        <div className="w-full flex flex-col gap-4">
+                            {question.options.map((answer, index) => (
+                                <div
+                                    key={index}
+                                    className="flex items-center py-4 px-5 space-x-2 border-2 bg-base-content/5 border-base-content/10 rounded-xl"
+                                >
+                                    <input type="radio" className="w-6 h-6" />
+                                    <p>{answer}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </section>
     );
 };
 
