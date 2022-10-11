2d11d442632f1681749cc9b5e82a56b7514ca2fd
MD Pahlovi <mdpahlovi07@gmail.com>
2022-10-11 19:54:52 +0600

    Toast Added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BiShow } from "react-icons/bi";
 
-const Question = ({ question, questionNo, totalQuestion, setAnswer }) => {
-    const { correctAnswer } = question;
+const Question = ({ question, questionNo, totalQuestion, setAnswer, setOptionAndAnswer }) => {
+    const { correctAnswer, options } = question;
+
+    // String TO HTML
     function srtingToHtml() {
         return { __html: question.question };
     }
+
     return (
         <div className="bg-base-100 px-5 py-5 shadow-sm shadow-base-content rounded-xl flex flex-col items-start w-full">
             <h4 className="mt-1 text-xl text-base-content/60 w-full flex justify-between items-start">
@@ -20,12 +23,18 @@ const Question = ({ question, questionNo, totalQuestion, setAnswer }) => {
             </h4>
             <div dangerouslySetInnerHTML={srtingToHtml()} className="my-4 text-2xl"></div>
             <div className="w-full flex flex-col gap-4">
-                {question.options.map((answer, index) => (
+                {options.map((answer, index) => (
                     <div
                         key={index}
                         className="flex items-center py-4 px-5 space-x-2 border-2 bg-base-content/5 border-base-content/10 rounded-xl"
                     >
-                        <input type="radio" className="w-6 h-6" />
+                        <input
+                            onClick={() =>
+                                setOptionAndAnswer({ selectedOption: options[index], correctAnswer: correctAnswer })
+                            }
+                            type="radio"
+                            className="w-6 h-6"
+                        />
                         <p>{answer}</p>
                     </div>
                 ))}
--- src/components/QuizDetails.js
+++ src/components/QuizDetails.js
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-
 import Modal from "./Modal";
 import Question from "./Question";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const QuizDetails = () => {
     const { data } = useLoaderData();
-    const [answer, setAnswer] = useState("");
     const { name, questions } = data;
+
+    // Show answer
+    const [answer, setAnswer] = useState("");
+
+    // Show toast
+    const [optionAndAnswer, setOptionAndAnswer] = useState({});
+    const { selectedOption, correctAnswer } = optionAndAnswer;
+    if (selectedOption && correctAnswer) {
+        if (selectedOption === correctAnswer) {
+            toast.success("🦄 Wow so easy!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        } else {
+            toast.error("🦄 Wow so easy!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+    }
+
     return (
         <>
             <section className="mt-16 lg:mt-20 my-cotainer section-gap content-gap">
@@ -22,11 +55,25 @@ const QuizDetails = () => {
                             questionNo={index + 1}
                             totalQuestion={questions.length}
                             setAnswer={setAnswer}
+                            setOptionAndAnswer={setOptionAndAnswer}
                         ></Question>
                     ))}
                 </div>
             </section>
             <Modal answer={answer}></Modal>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ToastContainer />
         </>
     );
 };
